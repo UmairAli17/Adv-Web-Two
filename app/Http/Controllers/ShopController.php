d0927b5481499ee38ace4e76d808f67d8ab1a508
Umair Ali <umairali17@hotmail.co.uk>
2017-04-12 17:04:57 +0100

    Implement File (Image Upload): Shopowner Can Now Upload Logo for Shop

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -9,35 +9,61 @@ use App\Businesses;
 
 class ShopController extends Controller
 {
+
+    /**
+     * [Display Shopkeeper Dashboard]
+     * @return [type] [description]
+     */
     public function dashboard()
     {
     	$shop =  Auth::user()->load('business');
     	return view('shop.dash', compact('shop'));
     }
 
+
+    /**
+     * [Display Shopkeeper's Shop(Business) Profile]
+     * @param  [string] $shop [id]
+     * @return [type]       [description]
+     */
     public function profile($shop)
     {
     	$business = Businesses::findOrFail($shop);
     	return $business;
     }
 
+
+    /**
+     * [edit_shop description]
+     * @param  [type] $shop [description]
+     * @return [type]       [description]
+     */
     public function edit_shop($shop)
     {
     	$shops = Businesses::findOrFail($shop);
     	return view('shop.edit', compact('shops'));
 	}
 
+
+    /**
+     * [Update the Shopkeer's Shop (Business). Allow for File Upload]
+     * @param  Request $request [Request]
+     * @param  [type]  $shop    [Business Model instance]
+     * @return [type]
+     */
 	public function update_shop(Request $request, $shop)
     {
     	$shops = Businesses::findOrFail($shop);
-    	$file = $request->image;
-    	if($file = $request->image;)
+    	if($file = $request->hasFile('image'))
     	{
             $file = $request->file('image');
             $name = time() . '-' . $file->getClientOriginalName();
             $file->move(public_path().'/uploads/logo', $name);
-    		$shops['image'] = $name;
+    		$shops = Businesses::where('id', $shop)->update(['image' => $name,]);
     	}
-        $shops->update($request->all());
+        else{
+            $shops->update($request->all());
+        }
+        return back();
 	}
 }
